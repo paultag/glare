681bc52f1af36949aa534e5e285da89b9fc27c43
Paul Tagliamonte <tag@pault.ag>
2013-11-23 10:39:21 -0500

    Add bundle bits.

--- glare/cards.py
+++ glare/cards.py
@@ -6,16 +6,35 @@ from glass.models import DeletableGlassTimelineItem, NotifiableGlassTimelineItem
 
 class TemplatedTimelineItem(DeletableGlassTimelineItem, NotifiableGlassTimelineItem):
     TEMPLATE = None
+    BUNDLE = None
+    COVER = False
 
     def __init__(self, **kwargs):
         if self.TEMPLATE is None:
             raise NotImplementedError("Erm, can't find the template")
+
         html = render_html(self.TEMPLATE, **kwargs)
-        super(TemplatedTimelineItem, self).__init__(html=html)
+        kwargs = {"html": html}
+
+        if self.BUNDLE is not None:
+            kwargs['bundleId'] = self.BUNDLE
+
+        if self.COVER is not None:
+            kwargs['isBundleCover'] = self.COVER
+
+        super(TemplatedTimelineItem, self).__init__(**kwargs)
 
 
 class LegislatorTimelineItem(TemplatedTimelineItem):
     TEMPLATE = "glare/cards/legislator.html"
+    BUNDLE = "sunlight-glare"
+    COVER = False
+
+
+class LegislatorCoverItem(TemplatedTimelineItem):
+    TEMPLATE = 'glare/cards/cover.html'
+    BUNDLE = "sunlight-glare"
+    COVER = True
 
 
 def render_html(path, **kwargs):
--- glare/management/commands/oob.py
+++ glare/management/commands/oob.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from glass.social_auth import SocialGlassAPI
-from glare.cards import LegislatorTimelineItem
+from glare.cards import LegislatorTimelineItem, LegislatorCoverItem
 from sunlight import openstates
 import uuid
 import sys
@@ -20,6 +20,13 @@ class Command(BaseCommand):
         latest = location.get_current_location()
 
         legislators = openstates.legislator_geo_search(latest.lat, latest.lon)
+
+        for item in timeline:
+            timeline.delete_item(item.id)
+
+        cover = LegislatorCoverItem(legs=[x['full_name'] for x in legislators])
+        id = timeline.add_item(cover)
+        print id
 
         for legislator in legislators:
             l = LegislatorTimelineItem(leg=legislator)
